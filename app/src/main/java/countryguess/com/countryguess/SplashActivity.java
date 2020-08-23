ca574161e258d4304c5126a8f0438fffcc9ebefa
Anirudh Anantharamakrishnan <anirudhkrishnan.1993@gmail.com>
2020-08-23 11:01:53 +0530

    Replaced the ugly splash screen with more subtle spinning globe on a peaceful white background.

--- app/src/main/java/countryguess/com/countryguess/SplashActivity.java
+++ app/src/main/java/countryguess/com/countryguess/SplashActivity.java
@@ -16,31 +16,17 @@ import android.widget.ImageView;
 public class SplashActivity extends AppCompatActivity {
 
     private final int SPLASH_DISPLAY_LENGTH = 3000;
-    ImageView globe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        globe = (ImageView) findViewById(R.id.globe);
-        scaleView(globe,0f,1f);
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
                 Intent mainIntent = new Intent(SplashActivity.this,GameActivity.class);
-                SplashActivity.this.startActivity(mainIntent);
-                SplashActivity.this.finish();
+                startActivity(mainIntent);
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
-    public void scaleView(View v, float startScale, float endScale) {
-        Animation anim = new ScaleAnimation(
-                1f, 1f, // Start and end values for the X axis scaling
-                startScale, endScale, // Start and end values for the Y axis scaling
-                Animation.RELATIVE_TO_SELF, 0f, // Pivot point of X scaling
-                Animation.RELATIVE_TO_SELF, 1f); // Pivot point of Y scaling
-        anim.setFillAfter(true); // Needed to keep the result of the animation
-        v.startAnimation(anim);
-    }
-
 }
